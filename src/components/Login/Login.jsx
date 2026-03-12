57ab2cf71d503c0f0133a0cd7b6cdaa378cdac36
Amina <minassouane@gmail.com>
2026-03-12 22:47:18 +0100

    feat(auth): add admin login logic

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,58 @@
+import styles from "./Login.module.css";
+import { useState } from "react";
+
+export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) throw Error;
+
+      const data = await response.json;
+      localStorage.setItem("token", data.token);
+      console.log("Logged in!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            required
+          />
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+          />
+          <button type="submit">Login</button>
+        </fieldset>
+      </form>
+    </>
+  );
+}
